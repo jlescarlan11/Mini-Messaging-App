164301fc3e79f1357230f20a00d7ef20a73e4f41
jlescarlan11 <jlescarlan11@gmail.com>
2025-04-06 11:53:55 +0800

    added database.

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-// Correct connection string construction
 module.exports = new Pool({
-  connectionString: `postgresql://${process.env.DB_USER}:${encodeURIComponent(
-    process.env.DB_PASSWORD
-  )}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // Required for Railway's PostgreSQL
   },
