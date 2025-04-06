168d7f8cde938e739a6432592bf44d449b68f66c
jlescarlan11 <jlescarlan11@gmail.com>
2025-04-06 11:46:57 +0800

    added database.

--- db/pool.js
+++ db/pool.js
@@ -6,4 +6,7 @@ module.exports = new Pool({
   connectionString: `postgresql://${process.env.DB_USER}:${encodeURIComponent(
     process.env.DB_PASSWORD
   )}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  ssl: {
+    rejectUnauthorized: false, // Required for Railway's PostgreSQL
+  },
 });
