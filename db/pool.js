57c59b8296946fa52fb4d7a3ec3da5507d971cd3
jlescarlan11 <jlescarlan11@gmail.com>
2025-04-06 12:16:26 +0800

    added database.

--- db/pool.js
+++ db/pool.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 ("/pleease");
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 });
